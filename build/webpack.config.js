eab19af700bf4e401c973113de0ad0bf46f14521
yc111 <yinchuan_111@163.com>
2020-03-05 16:50:33 +0800

    optimised webpack config

---
+++ build/webpack.config.js
@@ -0,0 +1,63 @@
+const merge = require('webpack-merge');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const libConfig = require('./webpack.lib.conf');
+const siteConfig = require('./webpack.site.conf');
+
+module.exports = (env) => {
+    const baseConfig = {
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    use: 'vue-loader'
+                },
+                {
+                    test: /\.js$/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.less$/,
+                    use: ['style-loader','css-loader', 'less-loader']
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(png|jpe?g|gif|bmp)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options:{
+                                limit: 1024 * 8,
+                                esModule: false,
+                                outputPath: 'images'
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(eot|svg|ttf|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                esModule: false,
+                                outputPath: 'fonts'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+        ]
+    };
+    if(env.lib) {
+        return merge(baseConfig, libConfig)
+    } 
+    if(env.site) {
+        return merge(baseConfig, siteConfig)
+    }
+}
---
+++ build/webpack.site.conf.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
