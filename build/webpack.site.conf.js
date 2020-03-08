52822536c5a13e55d2530ebe6f7ddcdb10ed3736
yc111 <yinchuan_111@163.com>
2020-03-08 18:10:44 +0800

    optimized site pack

--- build/webpack.site.conf.js
+++ build/webpack.site.conf.js
@@ -1,14 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
+  mode: 'production',
+  // devtool: 'source-map',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].js',
+  },
+  optimization: {
+    minimize: true,
+    usedExports: true,
+    sideEffects: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -16,5 +23,9 @@ module.exports = {
       template: './public/index.html',
       filename: 'index.html',
     }),
+    new CopyWebpackPlugin([
+      { from: './public/*.png', to: '[name].png' },
+    ]),
+    new CompressionWebpackPlugin(),
   ],
 };
--- src/directives/highlight.js
+++ src/directives/highlight.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
 import 'highlight.js/styles/gruvbox-light.css';
+
+hljs.registerLanguage('javascript', javascript);
 
 Vue.directive('highlight', (el) => {
   const blocks = el.querySelectorAll('code');
